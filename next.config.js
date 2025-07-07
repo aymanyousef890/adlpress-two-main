3ec58ca371b34932800096dd82876c300a1069b4
aymanyousef890 <91222918+aymanyousef890@users.noreply.github.com>
2025-07-07 17:26:14 +0300

    fin

--- next.config.js
+++ next.config.js
@@ -1,30 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
-  i18n: {
-    locales: ["ar", "en"],
-    defaultLocale: "ar",
-    localeDetection: false
-  },
-  images: {
-    domains: ["pasteboard.co","images.unsplash.com", "s3-alpha-sig.figma.com", "res.cloudinary.com"]
-  },
-  async redirects() {
-    return [
-      // إعادة توجيه جميع الروابط غير الموجودة للصفحة الرئيسيةبيستنبا 
-      {
-        source: '/:path*',
-        destination: '/',
-        permanent: false,
-        has: [
-          {
-            type: 'header',
-            key: 'x-middleware-rewrite',
-          },
-        ],
-      },
-    ]
-  },
+  output: 'export',
 }
 
 module.exports = nextConfig
