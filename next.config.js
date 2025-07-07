1ec972453af8b0ccdd47f346895af33701224793
aymanyousef890 <91222918+aymanyousef890@users.noreply.github.com>
2025-07-07 16:53:37 +0300

    finalsolve

--- next.config.js
+++ next.config.js
@@ -1,25 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- output:"export",
-  images: {
-    domains: ["pasteboard.co","images.unsplash.com", "s3-alpha-sig.figma.com", "res.cloudinary.com"]
-  },
-  async redirects() {
-    return [
-      // إعادة توجيه جميع الروابط غير الموجودة للصفحة الرئيسيةبيستنبا 
-      {
-        source: '/:path*',
-        destination: '/',
-        permanent: false,
-        has: [
-          {
-            type: 'header',
-            key: 'x-middleware-rewrite',
-          },
-        ],
-      },
-    ]
-  },
+ output:"export"
 }
 
 module.exports = nextConfig
