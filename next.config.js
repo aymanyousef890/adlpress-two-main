d371bcd01bbcb6f2afe4644bf39b2aa999b6f2cc
aymanyousef890 <91222918+aymanyousef890@users.noreply.github.com>
2025-07-07 16:40:42 +0300

    edit1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output:"export",
   reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
   i18n: {
     locales: ["ar", "en"],
